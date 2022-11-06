b6c48d8b367724fd73d0373e6c31fde7f9ffa435
Souvik <roysouvik5054@gmail.com>
2022-11-06 21:46:43 +0530

    first commit

---
+++ components/Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Link from 'next/link'
+const Product = ({product: {image,name,slug,price}}) => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
